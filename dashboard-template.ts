515ac20ec3bc20b63247c60a6c1a9440120615bd
Himanshu Barot <hbarot89@gmail.com>
2022-07-22 20:09:17 +0530

    Add files via upload

---
+++ dashboard-template.ts
@@ -0,0 +1,30 @@
+
+export default class dashboardtemplate {
+
+    public static dashboardHTML: string =
+        `
+        <div class="container">
+        <h2>Custom Form</h2>
+        <p>Custom Form in SPFX using Bootstrap 4</p>
+        <form>
+        <div class="form-group">
+            <label for="inputdefault">Name</label>
+            <input class="form-control" id="inputdefault" type="text">
+        </div>
+        <div class="form-group">
+            <label for="sel1">Country</label>
+            <select class="form-control" id="sel1">
+                <option>-Select-</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Submit</button>
+        </div>
+        </form>
+    </div>
+ 
+    `
+}
