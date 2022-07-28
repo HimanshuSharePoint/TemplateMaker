c7397035d02b6ffc3ecb00f76713b5e905bd45c9
Himanshu Barot <hbarot89@gmail.com>
2022-07-28 21:27:22 +0530

    Update Winnable-Proposal-Planner.js

--- Proposal PLanner/Winnable-Proposal-Planner.js
+++ Proposal PLanner/Winnable-Proposal-Planner.js
@@ -1,6 +1,7 @@
 // JavaScript source code
 var Winnable_PlannerProposal_BAL = {
     initializePage: function () {
+	    alert("This is from External Planner Proposal");
 	    renderPageControls();
     }
 };
