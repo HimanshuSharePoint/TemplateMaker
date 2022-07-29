35706788904b406b2dfa3023a23f81a9b4fc3f7a
Himanshu Barot <hbarot89@gmail.com>
2022-07-29 20:37:47 +0530

    Add files via upload

---
+++ Proposal PLanner/ProjectDetail/Winnable-Project-Details-BAL.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+var Winnable_ProjectDetails_BAL = {
+	initializePage: function () {
+		alert("This is from External Project Details");
+		renderPageControls();
+	}
+};
+function renderPageControls() {
+	console.log("test");
+}
