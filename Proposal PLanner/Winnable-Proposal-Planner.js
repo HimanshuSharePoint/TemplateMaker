913d7900bc9fc1f0a8d77cc72ca255ee783d3ff1
Himanshu Barot <hbarot89@gmail.com>
2022-08-05 00:30:36 +0530

    Update Winnable-Proposal-Planner.js

--- Proposal PLanner/Winnable-Proposal-Planner.js
+++ Proposal PLanner/Winnable-Proposal-Planner.js
@@ -1,9 +1,40 @@
 var Winnable_PlannerProposal_BAL = {
-    initializePage: function () {
-	    alert("This is from External Planner Proposal");
-	    renderPageControls();
-    }
+   initializePage: function () {
+      alert("This is from External Planner Proposal");
+      renderPageControls();
+   }
 };
-function renderPageControls(){
-	console.log("test");
+
+function renderPageControls() {
+
+   $(document).on("click", "#btncreateNewOpportunity", function () {
+      try {
+         $("#kendoCreateProposal").kendoWindow({
+            actions: ["Close"],
+            modal: true,
+            visible: false,
+            title: "Create Task",
+            close: this.onCloseKendoFade
+         }).data("kendoWindow").center().open();
+
+      } catch (ex) {
+         console.log("Error while btnProposalView" + ex.message);
+      }
+   });
+
+   $("#kendoCreateProposal").kendoWindow({
+      actions: ["Close"],
+      modal: true,
+      visible: false,
+      title: "Create Task",
+      close: this.onCloseKendoFade
+   }).data("kendoWindow").center();
+}
+
+function onCloseKendoFade() {
+   try {
+      $("#kendoCreateProposal").fadeIn()
+   } catch (ex) {
+      console.log("Error while onCloseKendoFade" + ex.message);
+   }
 }
