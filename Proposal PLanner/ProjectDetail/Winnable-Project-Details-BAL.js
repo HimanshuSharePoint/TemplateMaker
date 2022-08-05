56f435dbaf808d02eed2b8f13c38234a52fedc56
Chintan Sanghavi <83451978+chintanksanghavi@users.noreply.github.com>
2022-08-06 01:24:26 +0530

    Update Winnable-Project-Details-BAL.js

--- Proposal PLanner/ProjectDetail/Winnable-Project-Details-BAL.js
+++ Proposal PLanner/ProjectDetail/Winnable-Project-Details-BAL.js
@@ -2,7 +2,7 @@
 var Winnable_ProjectDetails_BAL = {
 	initializePage: function () {
 		alert("This is from External Project Details");
-		initializeMetronicScriptBundle();
+		
 		renderPageControls();
 	}
 };
@@ -10,16 +10,4 @@ function renderPageControls() {
 	console.log("test");
 	
 }
-function initializeMetronicScriptBundle()
-{
-KTMenu.createInstances();
-KTScroll.createInstances();
-KTScrolltop.createInstances();
-KTSticky.createInstances();
-KTDialer.createInstances();
-KTImageInput.createInstances();
-KTPasswordMeter.createInstances();
-KTSwapper.createInstances();
-KTToggle.createInstances();
 
-}
