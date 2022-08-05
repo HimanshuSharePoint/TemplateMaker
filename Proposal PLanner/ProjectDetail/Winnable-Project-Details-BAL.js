1b632f66ed8c11610b7805097eb2d91104cc9da3
Himanshu Barot <hbarot89@gmail.com>
2022-08-05 23:28:47 +0530

    Update Winnable-Project-Details-BAL.js

--- Proposal PLanner/ProjectDetail/Winnable-Project-Details-BAL.js
+++ Proposal PLanner/ProjectDetail/Winnable-Project-Details-BAL.js
@@ -2,15 +2,30 @@
 var Winnable_ProjectDetails_BAL = {
 	initializePage: function () {
 		alert("This is from External Project Details");
+		initializeMetronicScriptBundle();
 		renderPageControls();
 	}
 };
 function renderPageControls() {
 	console.log("test");
-	renderScriptBundle();
-}
-function renderScriptBundle()
-{ 
 	
+}
+function initializeMetronicScriptBundle()
+{ 
+	KTApp.init();// Boostrap & 3rd-party components dynamic creation
+KTApp.createInstances();// Layout partials initialization(if applicable)
+KTAppSidebar.init();
+KTAppSearch.init();
+
+KTMenu.createInstances();
+KTDrawer.createInstances();
+KTScroll.createInstances();
+KTScrolltop.createInstances();
+KTSticky.createInstances();
+KTDialer.createInstances();
+KTImageInput.createInstances();
+KTPasswordMeter.createInstances();
+KTSwapper.createInstances();
+KTToggle.createInstances();
 
 }
