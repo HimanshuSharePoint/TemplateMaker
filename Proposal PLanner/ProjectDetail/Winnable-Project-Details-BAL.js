f6db207e10a6ab61e20344327c7cf0c504a5bb59
Himanshu Barot <hbarot89@gmail.com>
2022-08-06 10:48:33 +0530

    Update Winnable-Project-Details-BAL.js

--- Proposal PLanner/ProjectDetail/Winnable-Project-Details-BAL.js
+++ Proposal PLanner/ProjectDetail/Winnable-Project-Details-BAL.js
@@ -12,40 +12,4 @@ function renderPageControls() {
 	
 }
 
-function initializeMetronicScriptBundle()
-{
-	
-	
-KTMenu.init();
-KTMenu.createInstances();
 
-KTDrawer.init();
-KTDrawer.createInstances();
-
-KTScroll.init();
-KTScroll.createInstances();
-
-KTScrolltop.init();
-KTScrolltop.createInstances();
-
-KTSticky.init();
-KTSticky.createInstances();
-
-KTDialer.init();
-KTDialer.createInstances();
-
-KTImageInput.init();
-KTImageInput.createInstances();
-
-KTPasswordMeter.init();
-KTPasswordMeter.createInstances();
-
-KTSwapper.init();
-KTSwapper.createInstances();
-
-KTToggle.init();
-KTToggle.createInstances();
-
-
-	
-}
