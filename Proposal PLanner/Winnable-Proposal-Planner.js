23f6c06bce1d96c04a66bfce76d426bb1c7abe24
Chintan Sanghavi <83451978+chintanksanghavi@users.noreply.github.com>
2022-08-07 17:50:15 +0530

    Update Winnable-Proposal-Planner.js

--- Proposal PLanner/Winnable-Proposal-Planner.js
+++ Proposal PLanner/Winnable-Proposal-Planner.js
@@ -1,12 +1,10 @@
 var Winnable_PlannerProposal_BAL = {
    initializePage: function () {
-      alert("This is from External Planner Proposal");
       renderPageControls();
    }
 };
 
 function renderPageControls() {
-
    console.log("This is Test");
 };
 
